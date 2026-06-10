57407b746cc043577b4bbc6b0e79164e0a4b86d9
ashishak2505 <ashishak2505@gmail.com>
2026-06-10 13:18:27 +0530

    fixed vactor store and embedding services

--- app/rag/vector_store.py
+++ app/rag/vector_store.py
@@ -1,12 +1,22 @@
 import chromadb
 
-client = chromadb.PersistentClient(
-    path="chroma_db"
-)
+_client = None
+_collection = None
 
-collection = client.get_or_create_collection(
-    name="medical_reports"
-)
+
+def get_collection():
+    global _client, _collection
+
+    if _collection is None:
+        _client = chromadb.PersistentClient(
+            path="chroma_db"
+        )
+
+        _collection = _client.get_or_create_collection(
+            name="medical_reports"
+        )
+
+    return _collection
 
 
 def store_embedding(
@@ -15,6 +25,7 @@ def store_embedding(
     text,
     report_id
 ):
+    collection = get_collection()
 
     collection.add(
         ids=[chunk_id],
--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,14 +1,17 @@
-from sentence_transformers import (
-    SentenceTransformer
-)
+from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+model = None
+
+def get_model():
+    global model
+
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    return model
 
 
 def create_embedding(text):
-
+    model = get_model()
     embedding = model.encode(text)
-
     return embedding.tolist()
